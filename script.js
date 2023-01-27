303c6c5ef56f7ec1f6e76b24d65391f33618c725
Ajay Karki <115032238+Ajaykarki13@users.noreply.github.com>
2023-01-28 00:32:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ function threeSum(arr, target) {
   //your code here
 	let sum = 0;
 	let array =[]
-	let min=Number.MIN_SAFE_INTEGER;
+	let min=Number.MAX_VALUE;
 	for(let i=0;i<arr.length-2;i++)
 		{
 for(let j=i;j<=i+2;j++)
 	{
 		sum=sum + arr[j]
 	}
-		let	diff= sum-target;
+		let	diff= Math.abs(sum-target)
 			array.push(diff);
 		}
 	for(let i=0;i<array.length;i++)
