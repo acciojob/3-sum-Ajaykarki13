731da8374dab3ece691a8e6cc03ab878d3462fc1
Ajay Karki <115032238+Ajaykarki13@users.noreply.github.com>
2023-01-28 00:11:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function threeSum(arr, target) {
   //your code here
+	let sum = 0;
+	let array =[]
+	let min=Integer.maxvalue;
+	for(let i=0;i<arr.length-2;i++)
+		{
+for(let j=i;j<=i+2;j++)
+	{
+		sum=sum + arr[j]
+	}
+		let	diff= sum-target;
+			array.push(diff);
+		}
+	for(let i=0;i<array.length;i++)
+		{
+			min = Math.min(array[i],min)
+		}
+	return min;
 }
 
 module.exports = threeSum;
